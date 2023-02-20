c2e30650d7153815aed214188143f2437e63688e
Kamil <khamiga97@gmail.com>
2023-02-20 20:43:53 +0100

    Problem with JSON solved

--- cypress/e2e/ts1.cy.js
+++ cypress/e2e/ts1.cy.js
@@ -5,17 +5,19 @@ describe('my first scenario', () => {
     const homePage = new HomePage();
     const accountPage = new AccountPage();
 
-    before(function () {
-        cy.fixture('users').as('userData')
-    })
+  before(function () {
+    cy.fixture('users').then(function(users) {
+      this.userData = users;
+    });
+  });
 
-    it('should login to the application', () => {
-        homePage.visitHomePage()
-        homePage.clickMyAccountButton()
-        accountPage.fillUsernameFieldWithEmail(this.userData.email)
-        accountPage.fillPasswordField(this.userData.password)
-        accountPage.clickLoginButton()
-    })
+  it('should login to the application', function () {
+    homePage.visitHomePage()
+    homePage.clickMyAccountButton()
+    accountPage.fillUsernameFieldWithEmail(this.userData.email)
+    accountPage.fillPasswordField(this.userData.password)
+    accountPage.clickLoginButton()
+  });
 
     it('should not login to the application', () => {
 
