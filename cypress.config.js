abb9fe8a98948a02780761afbba845e136f6095f
Kamil <khamiga97@gmail.com>
2023-02-27 20:30:31 +0100

    Dashboard connected

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'p2d1fd',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
